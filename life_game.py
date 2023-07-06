ae9fcb1fe8bc2b97b76b3e535a78f60fcbdbec77
Malchik1234 <ag-developer@smartpetrol.ru>
2023-07-06 21:08:55 +0300

    fix

--- life_game.py
+++ life_game.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+
 import random
 
 app = FastAPI()
@@ -15,42 +14,41 @@ field = [[random.choice([0, 1]) for _ in range(field_width)] for _ in range(fiel
 async def get_game_of_life():
     # Обработка текущего изменения состояния поля
     new_field = [[0] * field_width for _ in range(field_height)]
-    while field != new_field:
-        for i in range(field_height):
-            for j in range(field_width):
-                live_neighbors = 0
-                # Подсчет количества живых соседей
-                for x in range(-1, 2):
-                    for y in range(-1, 2):
-                        if x == 0 and y == 0:
-                            continue
-                        if field[(i + x) % field_height][(j + y) % field_width] == 1:
-                            live_neighbors += 1
-                # Применение правил игры
-                if field[i][j] == 1 and live_neighbors in [2, 3]:
-                    new_field[i][j] = 1
-                elif field[i][j] == 0 and live_neighbors == 3:
-                    new_field[i][j] = 1
+    for i in range(field_height):
+        for j in range(field_width):
+            live_neighbors = 0
+            # Подсчет количества живых соседей
+            for x in range(-1, 2):
+                for y in range(-1, 2):
+                    if x == 0 and y == 0:
+                        continue
+                    if field[(i + x) % field_height][(j + y) % field_width] == 1:
+                        live_neighbors += 1
+            # Применение правил игры
+            if field[i][j] == 1 and live_neighbors in [2, 3]:
+                new_field[i][j] = 1
+            elif field[i][j] == 0 and live_neighbors == 3:
+                new_field[i][j] = 1
 
-        # Проверка условий окончания игры
-        if field == new_field:
-            return {"message": "Игра завершена"}
+    # Проверка условий окончания игры
+    if field == new_field:
+        return {"message": "Игра завершена"}
 
-        live_count = 0
+    live_count = 0
 
-        for i in new_field:
-            for item in i:
-                if item == 1:
-                    live_count += 1
+    for i in new_field:
+        for item in i:
+            if item == 1:
+                live_count += 1
 
-        if live_count == 0:
-            return {"message": "Игра завершена"}
+    if live_count == 0:
+        return {"message": "Игра завершена"}
 
-        # Обновление поля
-        field[:] = new_field
+    # Обновление поля
+    field[:] = new_field
 
-        print("\n".join(map(str, field)))
-        print("________________________")
-        print("\n".join(map(str, new_field)))
+    print("\n".join(map(str, field)))
+    print("________________________")
+    print("\n".join(map(str, new_field)))
 
     return "\n".join(map(str, new_field))
